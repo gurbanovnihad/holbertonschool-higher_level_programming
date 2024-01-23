84dcb4274c706a4063ec787b6367bfb4c2d49665
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-23 10:37:40 -0800

    2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    print(chr(letter), end = '')
+for order in range(ord('a'), ord('z') + 1):
+    letter = chr(order)
+    print(f"{letter}", end = '')
