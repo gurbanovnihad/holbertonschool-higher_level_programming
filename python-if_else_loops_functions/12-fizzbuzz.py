0889ebc121de080218016fceb165f76cac112a2e
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-24 12:02:24 -0800

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 
 def fizzbuzz():
     for n in range(1, 101):
-        if n % 3 == 0 and  n % 5 ==0:
+        if n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz", end=" ")
         elif n % 3 == 0:
             print("Fizz", end=" ")
-        elif n % 5 ==0:
+        elif n % 5 == 0:
             print("Buzz", end=" ")
         else:
             print(n, end=" ")
