8d66c8c35901ab7892da854a08f58505cc57a49b
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-24 12:33:44 -0800

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 
 for order in range(-122, -96):
     if abs(order) % 2 == 1:
-        print(chr(abs(order) - 32), end="")
+        print("{}".format(chr(abs(order) - 32)), end="")
     else:
-        print(chr(abs(order)), end="")
+        print("{}".format(chr(abs(order))), end="")
