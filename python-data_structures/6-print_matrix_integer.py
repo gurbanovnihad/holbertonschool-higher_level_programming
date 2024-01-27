156af056207087f99e6eb135facda80717a8afc3
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-27 00:32:09 -0800

    Error fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    end = " "
+    row_end = " "
     for row in matrix:
         for integer in row:
             if row.index(integer) == len(row) - 1:
-                end = ""
-            print("{:d}".format(integer), end=" ")
+                row_end = ""
+            print("{:d}".format(integer), end=row_end)
         print()
