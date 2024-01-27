8bf4c089bb84c794ec84f8f8868a30b1a5094146
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-27 00:28:59 -0800

    7-islower.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    end = " "
     for row in matrix:
         for integer in row:
+            if row.index(integer) == len(row) - 1:
+                end = ""
             print("{:d}".format(integer), end=" ")
         print()
