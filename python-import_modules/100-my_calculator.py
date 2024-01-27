2762ef26659adac27868e1800acf3dec4447bf98
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-27 07:27:25 -0800

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     b = int(argv[3])
     operator = argv[2]
 
-    if len(argv) > 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     if operator not in ["+", "-", "*", "/"]:
