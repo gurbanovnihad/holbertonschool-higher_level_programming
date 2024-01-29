2beb2d1222b2bfaa2e5f888501f46021dbf934c4
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-29 05:06:57 -0800

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
 
     best_score = 0
     item_key = ''
-    
+
     for key, value in a_dictionary.items():
         if value > best_score:
             best_score = value
