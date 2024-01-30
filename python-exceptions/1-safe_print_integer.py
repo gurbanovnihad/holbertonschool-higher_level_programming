87dce00977784ee5e45efe668dd46c4bcfda2e72
Nihad Gurbanov <7404@holbertonstudents.com>
2024-01-30 04:14:01 -0800

    1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if int(value):
+        if int(value) and value != True:
             print("{:d}".format(value))
             return True
     except ValueError:
