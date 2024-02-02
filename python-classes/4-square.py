03880ab3be709494be5812c9cc4be7e2e869e318
Nihad Gurbanov <7404@holbertonstudents.com>
2024-02-02 03:41:30 -0800

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,11 +6,11 @@ class Square:
     '''Square class'''
     def __init__(self, size=0):
         self.size = size
-    
+
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, size):
         self.__size = size
