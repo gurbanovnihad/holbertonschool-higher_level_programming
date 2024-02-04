5707b9558e92c25ad7d8c9b36ba56d21598bebed
Nihad Gurbanov <7404@holbertonstudents.com>
2024-02-04 07:57:46 -0800

    0-add_integer.py, tests/0-add_integer.txt

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+This module is for practising test driven development
+'''
+
+
+def add_integer(a, b=98):
+    '''
+    add_integer function adds to integer and returns as an integer
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number. Default is 98.
+
+    Returns:
+        sum of a and b.
+
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    '''
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+
+    result = a + b
+
+    return result
