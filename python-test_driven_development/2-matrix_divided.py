4fb8ce292316afa09040902cb9df6e20bb786462
Nihad Gurbanov <7404@holbertonstudents.com>
2024-02-04 10:21:07 -0800

    2-matrix_divided.py, tests/2-matrix_divided.txt

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -34,8 +34,8 @@ def matrix_divided(matrix, div):
             matrix_check = False
             break
     if not matrix_check:
-        raise TypeError("matrix must be a matrix (list of lists) of \
-                integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists)\
+                of integers/floats")
 
     # Check if each row has the same size
     size_check = True
