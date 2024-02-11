7d61f40f2f2537b695fb307c3f8c154b8bd70bf2
gurbanovnihad <7404@holbertonstudents.com>
2024-02-11 20:23:55 +0400

    10-student.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Module Doc'''
+
+
+class Student:
+    '''Class Doc'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            new_dict = {}
+            for key_dict in self.__dict__:
+                for key_list in attrs:
+                    if key_dict == key_list:
+                        new_dict[key_dict] = self.__dict__[key_list]
+            return new_dict
+        return self.__dict__
