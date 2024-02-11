d6e463bd75d5cecdcbee3256a2d0fc5d550d806b
gurbanovnihad <7404@holbertonstudents.com>
2024-02-11 14:44:56 +0400

    11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -13,4 +13,4 @@ class Square(Rectangle):
         self.__size = size
 
     def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+        return "[Square] {}/{}".format(self.__size, self.__size)
