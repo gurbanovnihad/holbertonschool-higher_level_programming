4f1bb17cd512c95c137b5ab9ad91feb16d7c3543
gurbanovnihad <7404@holbertonstudents.com>
2024-03-24 00:15:46 +0400

    left join sum

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -10,6 +10,7 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_genres.name AS name, SUM(tv_show_genres.rating) AS rating
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id=tv_show_ratings.show_id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
