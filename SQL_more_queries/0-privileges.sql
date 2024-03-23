ec3bd1f724e0ff579f4e2e09800f54e88b53a6b9
gurbanovnihad <7404@holbertonstudents.com>
2024-03-23 14:18:41 +0400

    list privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Write a script that lists all privileges of the MySQL users user_0d_1 and
 -- user_0d_2 on your server (in localhost).
-SELECT * FROM mysql.user WHERE user IN ('root','nihad')
+SELECT * FROM mysql.user WHERE user IN ('user_0d_1','user_0d_2');
